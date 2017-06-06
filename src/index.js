eb5b56945d72d964fbfbdb98e46a3b122bf693f2
James Carlson <jxxcarlson@gmail.com>
2017-06-06 09:21:27 -0400

    Disable require('ace-css/css/ace.css'); in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('ace-css/css/ace.css');
+// require('ace-css/css/ace.css');
 require('font-awesome/css/font-awesome.css');
 
 // Require index.html so it gets copied to dist
