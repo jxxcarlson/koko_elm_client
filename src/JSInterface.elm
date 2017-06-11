261126c4be5d61428e861f59dc5610cb2bb818f4
James Carlson <jxxcarlson@gmail.com>
2017-06-10 22:30:58 -0400

    Configure MathJax to use files installed by npm, and configure so that MathJax will run without an internet connection.

--- src/JSInterface.elm
+++
@@ -1,6 +0,0 @@
-port module JSInterface
-
-
--- https://hackernoon.com/how-elm-ports-work-with-a-picture-just-one-25144ba43cdd
-
--- port render : String -> Cmd msg
--- src/index.js
+++ src/index.js
@@ -23,65 +23,59 @@ var mountNode = document.getElementById('main');
 //             src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML">
 //     </script>
 
-var app = Elm.Main.fullscreen(
-    {
-        width:  window.innerWidth,
-        height: window.innerHeight
+  var app = Elm.Main.fullscreen(
+      {
+          width:  window.innerWidth,
+          height: window.innerHeight
+      }
+    );
+
+  document.getElementById("rendered_text2").style.visibility = "hidden";
+
+  app.ports.toJs.subscribe(function (str) {
+    console.log("From Elm: " + str);
+    var settings = JSON.parse(str)
+    console.log("JSON object = " + JSON.stringify(settings))
+
+    var reader_height = (settings.height - 180)+ "px"
+    var editor_height = (settings.height - 180)+ "px"
+
+    var reader_width = (0.4*settings.width - 65) + "px"
+    var reader_left = (0.20*settings.width + 0)+ "px"
+
+    var editor_width = (0.4*settings.width - 65) + "px"
+    var editor_left = (0.6*settings.width + 5)+ "px"
+    console.log("editor_width: " + editor_width)
+
+     switch(settings.page) {
+        case "HomePage":
+            document.getElementById("rendered_text2").style.visibility = "hidden";
+            break;
+        case "ReaderPage":
+            document.getElementById("rendered_text2").style.visibility = "visible";
+            document.getElementById("rendered_text2").style.left = reader_left;
+            document.getElementById("rendered_text2").style.width = reader_width;
+            document.getElementById("rendered_text2").style.height = reader_height;
+            break;
+        case "EditorPage":
+            document.getElementById("rendered_text2").style.visibility = "visible";
+            document.getElementById("rendered_text2").style.left = editor_left;
+            document.getElementById("rendered_text2").style.width = editor_width;
+            document.getElementById("rendered_text2").style.height = editor_height;
+            break;
+        default:
+            document.getElementById("rendered_text2").style.visibility = "hidden";
     }
-  );
 
-document.getElementById("rendered_text2").style.visibility = "hidden";
-
-app.ports.toJs.subscribe(function (str) {
-  console.log("From Elm: " + str);
-  var settings = JSON.parse(str)
-  console.log("JSON object = " + JSON.stringify(settings))
-
-  var reader_height = (settings.height - 180)+ "px"
-  var editor_height = (settings.height - 180)+ "px"
-
-  var reader_width = (0.4*settings.width - 65) + "px"
-  var reader_left = (0.20*settings.width + 0)+ "px"
-
-  var editor_width = (0.4*settings.width - 65) + "px"
-  var editor_left = (0.6*settings.width + 5)+ "px"
-  console.log("editor_width: " + editor_width)
-
-   switch(settings.page) {
-      case "HomePage":
-          document.getElementById("rendered_text2").style.visibility = "hidden";
-          break;
-      case "ReaderPage":
-          document.getElementById("rendered_text2").style.visibility = "visible";
-          document.getElementById("rendered_text2").style.left = reader_left;
-          document.getElementById("rendered_text2").style.width = reader_width;
-          document.getElementById("rendered_text2").style.height = reader_height;
-          break;
-      case "EditorPage":
-          document.getElementById("rendered_text2").style.visibility = "visible";
-          document.getElementById("rendered_text2").style.left = editor_left;
-          document.getElementById("rendered_text2").style.width = editor_width;
-          document.getElementById("rendered_text2").style.height = editor_height;
-          break;
-      default:
-          document.getElementById("rendered_text2").style.visibility = "hidden";
+    if (settings.online == true) {
+      app.ports.render.subscribe(function(rendered_text) {
+            document.getElementById('rendered_text2').innerHTML = rendered_text
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+      })
+    } else {
+      app.ports.render.subscribe(function(rendered_text) {
+            document.getElementById('rendered_text2').innerHTML = rendered_text
+      })
   }
 
-  if (settings.online == true) {
-    app.ports.render.subscribe(function(rendered_text) {
-          document.getElementById('rendered_text2').innerHTML = rendered_text
-          MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-    })
-  } else {
-    app.ports.render.subscribe(function(rendered_text) {
-          document.getElementById('rendered_text2').innerHTML = rendered_text
-    })
-}
-
 })
-
-  // if (settings.online == true) {
-  //  app.ports.render.subscribe(function(rendered_text) {
-  //        document.getElementById('rendered_text2').innerHTML = rendered_text
-  //        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-  //  }
