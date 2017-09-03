8d89359d9d19a47b1d0c733cb216e30c54541450
James Carlson <jxxcarlson@mac.com>
2017-09-03 19:24:27 -0400

    Better design for Basic.button

--- src/Views/Basic.elm
+++ src/Views/Basic.elm
@@ -9,28 +9,15 @@ import Html
 type alias AwesomeFont msg = Color.Color -> Int -> Html.Html msg
 
 
-button
-    : String
-    -> a
-    -> List (Element.Internal.Model.Attribute variation msg)
-    -> Element a variation msg
+
 button title style attrs  =
     el style ([ paddingLeft 8, height (px 30), width (px 200) ] ++ attrs) (label title style [])
 
-label
-    : String
-    -> style
-    -> List (Element.Internal.Model.Attribute variation msg)
-    -> Element style variation msg
+
 label text_ style attrs  =
     (el style ([ verticalCenter, height (px 25), paddingLeft 10] ++ attrs) (el style [verticalCenter] (text text_)))
 
 
-faIcon
-    : String
-    -> (Color.Color -> number -> Html.Html msg)
-    -> List (Element.Internal.Model.Attribute variation msg)
-    -> Element Styles variation msg
 faIcon title_ icon attrs =
   el NavBarActive
     ([height (px 25), width (px 25), alignBottom, title title_, verticalCenter ] ++ attrs)
