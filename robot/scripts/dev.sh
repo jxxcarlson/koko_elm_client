636d2d29b97477554980eb2c703e896943e4e33f
James Carlson <jxxcarlson@mac.com>
2017-09-13 18:30:52 -0400

    Remove duplicate MathJax code from index.html

--- robot/scripts/dev.sh
+++ robot/scripts/dev.sh
@@ -1,4 +1,11 @@
-echo "use dev configuration"
+color=`tput setaf 48`
+reset=`tput setaf 7`
+
+echo
+echo "${color}Use dev configuration.${reset}"
 cp ./robot/src/dev/Configuration.elm ./src/
 cp ./robot/src/dev/webpack.config.js ./src/
+
+echo
+echo "${color}Start webpack${reset}"
 yarn start
