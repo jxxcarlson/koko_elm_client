e5faf3976b0a093f4babc713a88a76801a0e8f9a
James Carlson <jxxcarlson@mac.com>
2017-11-12 12:23:27 -0500

    Fix tabular environment (still ignores format, e.g., {l l l}

--- tests/ParserTest.elm
+++ tests/ParserTest.elm
@@ -147,7 +147,27 @@ suite =
                             )
                 in
                     Expect.equal parsedInput expectedOutput
-        , test "(9) table" <|
+        , test "(T.1) tablerow" <|
+            \_ ->
+                let
+                    parsedInput =
+                        run tableRow "1 & 2 & 3\n"
+
+                    expectedOutput =
+                        Ok (LatexList ([ LXString "1", LXString "2", LXString "3" ]))
+                in
+                    Expect.equal parsedInput expectedOutput
+        , test "(T.1a) tablerow" <|
+            \_ ->
+                let
+                    parsedInput =
+                        run tableRow "Hydrogen & H & 1 & 1.008 \\\\\n"
+
+                    expectedOutput =
+                        Ok (LatexList ([ LXString "Hydrogen", LXString "H", LXString "1", LXString "1.008" ]))
+                in
+                    Expect.equal parsedInput expectedOutput
+        , test "(T.2) table" <|
             \_ ->
                 let
                     parsedInput =
@@ -173,6 +193,62 @@ suite =
                             )
                 in
                     Expect.equal parsedInput expectedOutput
+        , test "(T.3) table" <|
+            \_ ->
+                let
+                    input =
+                        Debug.log "input (T.3)"
+                            "\\begin{tabular}{l l l l}\nHydrogen & H & 1 & 1.008 \\\\\nHelium & He & 2 & 4.003 \\\\\nLithium & Li & 3 &  6.94 \\\\\nBeryllium & Be & 4 & 9.012 \\\\\n\\end{tabular}"
+
+                    parsedInput =
+                        run parse input
+
+                    expectedOutput =
+                        Ok
+                            (Environment "tabular"
+                                (LatexList
+                                    ([ LatexList
+                                        ([ LXString "Hydrogen", LXString "H", LXString "1", LXString "1.008" ])
+                                     , LatexList ([ LXString "Helium", LXString "He", LXString "2", LXString "4.003" ])
+                                     , LatexList ([ LXString "Lithium", LXString "Li", LXString "3", LXString "6.94" ])
+                                     , LatexList ([ LXString "Beryllium", LXString "Be", LXString "4", LXString "9.012" ])
+                                     ]
+                                    )
+                                )
+                            )
+                in
+                    Expect.equal parsedInput expectedOutput
+        , test "(T.3a) table" <|
+            \_ ->
+                let
+                    input =
+                        Debug.log "input (T.3a)"
+                            """\\begin{tabular}
+Hydrogen & H & 1 & 1.008 \\\\
+Helium & He & 2 & 4.003 \\\\
+Lithium & Li & 3 &  6.94 \\\\
+Beryllium & Be & 4 & 9.012 \\\\
+\\end{tabular}
+"""
+
+                    parsedInput =
+                        run parse input
+
+                    expectedOutput =
+                        Ok
+                            (Environment "tabular"
+                                (LatexList
+                                    ([ LatexList
+                                        ([ LXString "Hydrogen", LXString "H", LXString "1", LXString "1.008" ])
+                                     , LatexList ([ LXString "Helium", LXString "He", LXString "2", LXString "4.003" ])
+                                     , LatexList ([ LXString "Lithium", LXString "Li", LXString "3", LXString "6.94" ])
+                                     , LatexList ([ LXString "Beryllium", LXString "Be", LXString "4", LXString "9.012" ])
+                                     ]
+                                    )
+                                )
+                            )
+                in
+                    Expect.equal parsedInput expectedOutput
         , test "(L.1) label" <|
             \_ ->
                 let
@@ -190,7 +266,7 @@ suite =
                         run latexList "test \\code{foo}."
 
                     expectedOutput =
-                        Ok (Environment "equation" (LXString "\n\\label{uncertaintyPrinciple}\n\\left[ \\hat p, x\\right] = -i \\hbar\n"))
+                        Ok (LatexList ([ LXString "test", Macro "code" ([ LatexList ([ LXString "foo" ]) ]), LXString "." ]))
                 in
                     Expect.equal parsedInput expectedOutput
         ]
