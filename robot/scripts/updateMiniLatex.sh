5e12628f45ce64039d4af0b57d62de6dea4942c8
James Carlson <jxxcarlson@mac.com>
2017-11-14 12:28:46 -0500

    udpate miniLatexTest publisher script

--- robot/scripts/updateMiniLatex.sh
+++ robot/scripts/updateMiniLatex.sh
@@ -6,23 +6,36 @@ reset=`tput setaf 7`
 echo
 echo "${color}Copying source files to MiniLatex package${reset}"
 
-cp src/MiniLatex/Accumulator.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/Configuration.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/Differ.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/Driver.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/KeyValueUtilities.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/LatexDiffer.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/Accumulator.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/LatexState.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/Parser.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/ParserTools.elm ../MiniLatex/src/MiniLatex
-cp src/MiniLatex/Render.elm ../MiniLatex/src/MiniLatex
+cp src/MiniLatex/Accumulator.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/Configuration.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/Differ.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/Driver.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/KeyValueUtilities.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/LatexDiffer.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/Accumulator.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/LatexState.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/Parser.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/ParserTools.elm ../MiniLatex/src/MiniLatex/
+cp src/MiniLatex/Render.elm ../MiniLatex/src/MiniLatex/
+
+cp src/MiniLatex/Accumulator.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/Configuration.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/Differ.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/Driver.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/KeyValueUtilities.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/LatexDiffer.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/Accumulator.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/LatexState.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/Parser.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/ParserTools.elm ../MiniLatexTester/src/MiniLatex/
+cp src/MiniLatex/Render.elm ../MiniLatexTester/src/MiniLatex/
 
 
 echo
 echo "${color}Copying README to MiniLatex package${reset}"
 
 cp src/MiniLatex/README.MD ../MiniLatex/
+cp src/MiniLatex/README.MD ../MiniLatexTester/
 
 
 echo
@@ -33,6 +46,13 @@ cp tests/AccumulatorTest.elm ../MiniLatex/tests/
 cp tests/DifferTest.elm ../MiniLatex/tests/
 cp tests/ParserTest.elm ../MiniLatex/tests/
 cp tests/RenderTest.elm ../MiniLatex/tests/
+cp tests/DriverTest.elm ../MiniLatex/tests/
+
+cp tests/AccumulatorTest.elm ../MiniLatexTester/tests/
+cp tests/DifferTest.elm ../MiniLatexTester/tests/
+cp tests/ParserTest.elm ../MiniLatexTester/tests/
+cp tests/RenderTest.elm ../MiniLatexTester/tests/
+cp tests/DriverTest.elm ../MiniLatexTester/tests/
 
 echo
 echo "${color}Done${reset}"
