5c9f02371eff1da747dda16a18d1c1b2ba021f6f
James Carlson <jxxcarlson@mac.com>
2017-12-25 14:02:17 -0500

    Move Main.update to Update.Main.update

--- src/Main.elm
+++ src/Main.elm
@@ -1,69 +1,22 @@
 module Main exposing (..)
 
-import External exposing (fileUpload, fileUploaded, putTextToRender, toJs)
-import Init exposing (init)
+import Init
 import Nav.Parser
 import Navigation
 import Subscriptions exposing (subscriptions)
-import Types exposing (..)
-import Update.Auth
-import Update.Channel
-import Update.Document
-import Update.Image exposing (update)
-import Update.Page
-import Update.Periodic
-import Update.Search
-import Update.UI
-import Update.User
-import Update.Window
-import Views.Main exposing (view)
+import Types exposing (Flags, Model, Msg)
+import Update.Main
+import Views.Main
 
 
 main : Program Flags Model Msg
 main =
     Navigation.programWithFlags Nav.Parser.urlParser
-        { init = init
-        , view = view
-        , update = update
+        { init = Init.init
+        , view = Views.Main.view
+        , update = Update.Main.update
         , subscriptions = subscriptions
         }
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( { model | message = "NoOp" }, Cmd.none )
-
-        AuthMsg submessage ->
-            Update.Auth.update submessage model
-
-        ChannelMsg submessage ->
-            Update.Channel.update submessage model
-
-        DocMsg submessage ->
-            Update.Document.update submessage model
-
-        ImageMsg submessage ->
-            Update.Image.update submessage model
-
-        PageMsg submessage ->
-            Update.Page.update submessage model
-
-        PeriodicMsg submessage ->
-            Update.Periodic.update submessage model
-
-        SearchMsg submessage ->
-            Update.Search.update submessage model
-
-        UIMsg submessage ->
-            Update.UI.update submessage model
-
-        UserMsg submessage ->
-            Update.User.update submessage model
-
-        WindowMsg submessage ->
-            Update.Window.update submessage model
 
 
 
---
+++ src/Update/Main.elm
@@ -0,0 +1,50 @@
+module Update.Main exposing (update)
+
+import Types exposing (Model, Msg(..))
+import Update.Auth
+import Update.Channel
+import Update.Document
+import Update.Image
+import Update.Page
+import Update.Periodic
+import Update.Search
+import Update.UI
+import Update.User
+import Update.Window
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( { model | message = "NoOp" }, Cmd.none )
+
+        AuthMsg submessage ->
+            Update.Auth.update submessage model
+
+        ChannelMsg submessage ->
+            Update.Channel.update submessage model
+
+        DocMsg submessage ->
+            Update.Document.update submessage model
+
+        ImageMsg submessage ->
+            Update.Image.update submessage model
+
+        PageMsg submessage ->
+            Update.Page.update submessage model
+
+        PeriodicMsg submessage ->
+            Update.Periodic.update submessage model
+
+        SearchMsg submessage ->
+            Update.Search.update submessage model
+
+        UIMsg submessage ->
+            Update.UI.update submessage model
+
+        UserMsg submessage ->
+            Update.User.update submessage model
+
+        WindowMsg submessage ->
+            Update.Window.update submessage model
