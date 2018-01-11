c7e3e8ce3bd79b94f2d887e040a5944cb01cd93f
James Carlson <jxxcarlson@mac.com>
2018-01-11 18:14:12 +0800

    Refactor: put hasMath functions in HasMath module.

--- src/MiniLatex/Parser.elm
+++ src/MiniLatex/Parser.elm
@@ -25,54 +25,6 @@ type LatexExpression
 
 
 
-{- Has Math code -}
-
-
-{-| Determine whether a (List LatexExpression) has math text in it
--}
-listHasMath : List LatexExpression -> Bool
-listHasMath list =
-    list |> List.foldr (\x acc -> hasMath x || acc) False
-
-
-{-| Determine whether a LatexExpression has math text in it
--}
-hasMath : LatexExpression -> Bool
-hasMath expr =
-    case expr of
-        LXString str ->
-            False
-
-        Comment str ->
-            False
-
-        Item k expr ->
-            hasMath expr
-
-        InlineMath str ->
-            True
-
-        DisplayMath str ->
-            True
-
-        Macro str expr ->
-            expr |> List.foldr (\x acc -> hasMath x || acc) False
-
-        Environment str expr ->
-            envHasMath str expr
-
-        LatexList list ->
-            list |> List.foldr (\x acc -> hasMath x || acc) False
-
-
-{-| Determine whether an environment has math in it
--}
-envHasMath : String -> LatexExpression -> Bool
-envHasMath envType expr =
-    List.member envType [ "equation", "align", "eqnarray" ] || hasMath expr
-
-
-
 {- End of Has Math code -}
 
 
